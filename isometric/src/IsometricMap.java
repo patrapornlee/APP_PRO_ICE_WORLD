bc6a06ef5bfb205152e0f40fb0833032a3b94885
Thaninpong <thaninpong@gmail.com>
2013-04-11 00:05:20 +0700

    Make the tiles change color when the mouse entered

--- isometric/src/IsometricMap.java
+++ isometric/src/IsometricMap.java
@@ -1,30 +1,66 @@
 
 
+import java.applet.Applet;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.Polygon;
 import java.awt.Toolkit;
+import javax.swing.*;
 
 
 
-public class IsometricMap  {
-        
+public class IsometricMap extends JApplet  {
+	
+	private int MAX_X = Toolkit.getDefaultToolkit().getScreenSize().width;
+	private int MAX_Y = Toolkit.getDefaultToolkit().getScreenSize().height;
 	IsometricSquare square ;
+	Point p;
+	Image image;
+	Graphics buffer;
   
 
         public IsometricMap() {
-
+       
+        	this.setPreferredSize(new Dimension(MAX_X,MAX_Y));
+        
+        	Refresh refresh = new Refresh();
+        	refresh.start();
         }
 
-        public void paintMap(Graphics g) {
+        public void paint(Graphics g) {
+        	
+        	image = createImage (getWidth(),getHeight());
+        	buffer = image.getGraphics();
             
+        	p = getMousePosition();
                 for (int line = 0 ; line<100 ; line++) {
                 	for (int row = 100; row > 0; row --) {
                			square = new IsometricSquare (line,row);
-               			square.paintSquare(g);
-                
+               			if (p != null){
+               				if (square.contains(p)) {
+               					square.setHighlighted(true);
+               				}
+               				
+               			}
+               			square.paintSquare(buffer);
                 	}
                 }
+                g.drawImage(image, 0,0,this);
            	}
+        
+        class Refresh extends Thread{
+        	public void run(){
+        		while(true){
+	        			try{
+	        				sleep(40);
+	        			}catch(InterruptedException e){
+	        				
+	        			}
+        		repaint();
+        		}
+        	}
+        }
         
 }
--- isometric/src/IsometricSquare.java
+++ isometric/src/IsometricSquare.java
@@ -1,7 +1,12 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Polygon;
 import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JPanel;
 
 
 public class IsometricSquare {
@@ -11,25 +16,37 @@ public class IsometricSquare {
 	private int line, row, width, height;
 	private Point origin;
 	private Polygon poly;
+	private Boolean highlighted;
 	
 	public IsometricSquare (int line, int row) {
 		this.line = line;
 		this.row = row;
+		highlighted = false ;
+
 		width = MAX_X/200;
      	height = MAX_Y/200;
         origin = new Point (MAX_X/2,height+(MAX_Y-height*241)/2);
+        
+    	int originLine = origin.x - line*width ; 
+    	int originRow = origin.y + line*height;
+    	int[] px = {originLine + width*row, originLine + width*(row+1), originLine+ width*row, originLine + width*(row-1) };
+        int[] py = { originRow + height*(row-1), originRow + height*row, originRow + height*(row+1), originRow + height*row };
+        poly = new Polygon(px, py, 4);
+        
 	}
 	
     
 
 
     public void paintSquare(Graphics g) {
-        
-            	int originLine = origin.x - line*width ; 
-            	int originRow = origin.y + line*height;
-            	int[] px = {originLine + width*row, originLine + width*(row+1), originLine+ width*row, originLine + width*(row-1) };
-                int[] py = { originRow + height*(row-1), originRow + height*row, originRow + height*(row+1), originRow + height*row };
-                poly = new Polygon(px, py, 4);
+        		
+
+                
+                if (highlighted){
+                g.setColor(Color.RED);
+                g.fillPolygon(poly);}
+                
+                g.setColor(Color.BLACK);
         		g.drawPolygon(poly);
             }
     
@@ -39,6 +56,15 @@ public class IsometricSquare {
     
     public int getRow () {
     	return row;
+    }
+    
+    
+    public boolean contains ( Point p) {
+    	return poly.contains(p);
+    }
+
+    public void setHighlighted (boolean highlighted) {
+    	this.highlighted = highlighted;
     }
     
     }
