f72f0b0b8549d970f4882afc06f5cb1861885f93
Patraporn Leelaprachakul <himuken.kf@gmail.com>
2013-04-14 11:06:43 +0700

    Add login pages and pictures

---
+++ src/Connect.java
@@ -0,0 +1,14 @@
+import java.awt.datatransfer.StringSelection;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: AVALON
+ * Date: 7/4/2556
+ * Time: 21:38 น.
+ * To change this template use File | Settings | File Templates.
+ */
+public class Connect {
+    public boolean connect(String username, String password){
+       return true;
+    }
+}
